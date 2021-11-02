962cc1177cf08c5bd1fc721b731f4269a6a2cf5e
Shimon <shwosner@gmail.com>
2021-11-02 07:17:23 -0400

    updated logo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       templateColumns="max-content 1fr 80px"
       justifyItems="center"
       alignItems="center"
-      height="93px"
+      // height="93px"
       bg="white"
       position="sticky"
       top="0"
@@ -16,9 +16,9 @@ export default function Header() {
       borderBottom="20px solid #edf2f7"
     >
       <GridItem justifySelf="start" m="2">
-        <Image src="/rchat_logo.png" height="60px" />
+        <Image src="/public/logo.png" height="30px" ml="2" />
       </GridItem>
-      <GridItem justifySelf="end" alignSelf="center">
+      <GridItem justifySelf="end" alignSelf="end">
         {isGuest && <NameForm username={username} setUsername={setUsername} />}
         {!isGuest && (
           <>
@@ -26,7 +26,7 @@ export default function Header() {
           </>
         )}
       </GridItem>
-      <GridItem>
+      <GridItem alignSelf="end">
         {isGuest ? (
           <div
             style={{
