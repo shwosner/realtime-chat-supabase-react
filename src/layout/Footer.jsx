09caac8c9ecddc2ab8fa9c88db2b8567d8c3af2e
AVISHEKH RAJ <satyawansingh180241@gmail.com>
2025-04-02 03:57:31 +0530

    clean ui

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,42 +1,21 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Link, IconButton } from "@chakra-ui/react";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import MessageForm from "../components/MessageForm";
+
 export default function Footer() {
   return (
-    <Box position="fixed" bottom="0" width="100%">
+    <Box
+      position="fixed"
+      bottom="0"
+      width="100%"
+      bg="white"
+      boxShadow="md"
+      py={2}
+      px={4}
+    >
       <MessageForm />
-
-      <Grid
-        gridTemplateColumns="auto 1fr"
-        textAlign="center"
-        alignItems="center"
-        py="4px"
-        px="30px"
-        height="40px"
-        bg="white"
-      >
-        <GridItem justifySelf="start">
-          {/* Built by{" "} */}
-          {/* <a href="http://shimon-wosner.vercel.app" target="_blank"> */}
-          <a
-            href="https://twitter.com/shwosner"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaTwitter style={{ display: "inline" }} />
-            @shwosner
-          </a>
-        </GridItem>
-        <GridItem justifySelf="end">
-          <a
-            href="https://github.com/shwosner/realtime-chat-supabase-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub style={{ display: "inline" }} /> Source code
-          </a>
-        </GridItem>
-      </Grid>
+      
+     
     </Box>
   );
 }
